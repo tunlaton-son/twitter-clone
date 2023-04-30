69ad059a99a148890485f5600534845b8436e814
tunlaton-son <tunlaton@hotmail.com>
2023-04-30 20:39:47 +0700

    update package.json

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    DOMAIN: 'http://192.168.1.104',
-    NEXTAUTH_URL: "http://192.168.1.104",
-    VERCEL_URL:"http://192.168.1.104",
-  }
 }
 module.exports = nextConfig;
